b03ffe8e62883111af8695709e521db8c93a7142
Unknown <galih.setyawan24@mail.ugm.ac.id>
2019-04-28 22:37:48 +0700

    read serial only

--- SimpleSerialCommunication/Form1.Designer.cs
+++ SimpleSerialCommunication/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBaudrate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtConnect = new System.Windows.Forms.Button();
+            this.txtStream = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(120, 32);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(121, 21);
+            this.cmbPort.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+            // 
+            // cmbBaudrate
+            // 
+            this.cmbBaudrate.FormattingEnabled = true;
+            this.cmbBaudrate.Location = new System.Drawing.Point(120, 59);
+            this.cmbBaudrate.Name = "cmbBaudrate";
+            this.cmbBaudrate.Size = new System.Drawing.Size(121, 21);
+            this.cmbBaudrate.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Baudrate";
+            // 
+            // txtConnect
+            // 
+            this.txtConnect.Location = new System.Drawing.Point(120, 86);
+            this.txtConnect.Name = "txtConnect";
+            this.txtConnect.Size = new System.Drawing.Size(121, 23);
+            this.txtConnect.TabIndex = 3;
+            this.txtConnect.Text = "&Connect/Disconnect";
+            this.txtConnect.UseVisualStyleBackColor = true;
+            this.txtConnect.Click += new System.EventHandler(this.TxtConnect_Click);
+            // 
+            // txtStream
+            // 
+            this.txtStream.Location = new System.Drawing.Point(120, 115);
+            this.txtStream.Name = "txtStream";
+            this.txtStream.Size = new System.Drawing.Size(394, 20);
+            this.txtStream.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Data stream";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(527, 142);
+            this.Controls.Add(this.txtStream);
+            this.Controls.Add(this.txtConnect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbBaudrate);
+            this.Controls.Add(this.cmbPort);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbBaudrate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button txtConnect;
+        private System.Windows.Forms.TextBox txtStream;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- SimpleSerialCommunication/Form1.cs
+++ SimpleSerialCommunication/Form1.cs
@@ -7,14 +7,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace SimpleSerialCommunication
 {
     public partial class Form1 : Form
     {
+        SerialPort serialport = new SerialPort();
+        String DataStream ;
         public Form1()
         {
             InitializeComponent();
+            FillSerialParam();
+
+
+        }
+        private void FillSerialParam()
+        {
+            cmbPort.Items.Clear();
+            foreach (string serialname in SerialPort.GetPortNames())
+            {
+                cmbPort.Items.Add(serialname);
+            }
+            if (cmbPort.Items.Count > 0)
+                cmbPort.SelectedIndex = 0;
+            cmbBaudrate.Items.Add("1200");
+            cmbBaudrate.Items.Add("2400");
+            cmbBaudrate.Items.Add("4800");
+            cmbBaudrate.Items.Add("9600");
+            cmbBaudrate.Items.Add("19200");
+            cmbBaudrate.Items.Add("38400");
+            cmbBaudrate.Items.Add("57600");
+            cmbBaudrate.Items.Add("115200");
+            cmbBaudrate.Items.Add("230400");
+            cmbBaudrate.SelectedIndex = 3;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtConnect_Click(object sender, EventArgs e)
+        {
+            if (serialport.IsOpen)
+            {
+
+                serialport.DataReceived -= new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                serialport.Close();
+                
+            }
+            else
+            {
+                serialport.PortName = cmbPort.SelectedItem.ToString();
+                serialport.BaudRate = Convert.ToInt16(cmbBaudrate.SelectedItem);
+                serialport.DataBits = 8;
+                serialport.Parity = Parity.None;
+                serialport.StopBits = StopBits.One;
+
+                serialport.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                serialport.Open();
+            }
+            
+            
+        }
+
+        private void DataReceivedHandler(object sender,SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            //string indata = sp.ReadExisting();
+            //string indata = sp.ReadLine();
+            this.Invoke(new EventHandler(DoUpDate));
+            //Console.WriteLine("Data Received:");
+            //Console.Write(indata);
+        }
+
+        private void DoUpDate(object s, EventArgs e)
+        {
+            txtStream.Text = serialport.ReadLine().ToUpper();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialport.IsOpen)
+            {
+                serialport.Close();
+            }
         }
     }
 }
